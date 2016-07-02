f66ea771e23278d068565d5224f0af57cddc13d4
Luke Skibinski <lsh@ccxx.cx>
2016-07-02 18:16:48 +0100

    filled out the description to what the spec currently describes. placeholders for non-xml values that will need to be extracted another way. logging annotations for values that still need a little wrangling

--- src/main.py
+++ src/main.py
@@ -3,25 +3,71 @@ from et3.extract import path as p
 from et3.render import render
 from et3 import utils
 from elifetools import parseJATS
+from functools import partial
+import logging
+from collections import OrderedDict
+from datetime import datetime
+import time
+from slugify import slugify
+
+LOG = logging.getLogger(__name__)
 
 def article_list(doc):
     return [parseJATS.parse_document(doc)]
 
-def parser(funcname, *args, **kwargs):
+def jats(funcname, *args, **kwargs):
     def fn(soup):
         return getattr(parseJATS, funcname)(soup, *args, **kwargs)
     return fn
 
-description = {
-    'journal': {
-        'id': [1],
-        'title': ['eLife']
-    },
-    'article': {
-        'title': [parser('title')],
-        'impact-statement': [parser('impact_statement')],
-    }
-}
+def nonxml(attr):
+    LOG.warn("non-xml value %r" % attr)
+    return None
+
+def todo(msg):
+    def fn(val):
+        LOG.info(msg, extra={'value': val})
+        return val
+    return fn
+
+def to_isoformat(time_struct):
+    return datetime.fromtimestamp(time.mktime(time_struct)).isoformat()
+
+def category_codes(cat_list):
+    return [slugify(cat, stopwords=['and']) for cat in cat_list]
+
+description = OrderedDict([
+    ('journal', {
+        'id': [jats('journal_id')],
+        'title': [jats('journal_title')],
+        'issn': [jats('journal_issn', 'electronic')],
+    }),
+    ('article', OrderedDict([
+        ('id', [jats('publisher_id')]),
+        ('version', [nonxml('version')]),
+        ('type', [jats('article_type')]),
+        ('doi', [jats('doi')]),
+        ('title', [jats('title')]),
+        ('published', [jats('pub_date'), to_isoformat]),
+        ('volume', [jats('volume'), int]),
+        ('issue', [nonxml('issue')]),
+        ('elocationId', [jats('elocation_id')]),
+        ('copyright', OrderedDict([
+            ('licence', [jats('license'), todo('extract the licence code')]),
+            ('holder', [jats('copyright_holder')]),
+         ])),
+        ('pdf', [nonxml('pdf url')]),
+        ('subjects', [jats('category'), category_codes]),
+        ('research-organisms', [jats('research_organism')]),
+        ('related-articles', [jats('related_article')]),
+        ('abstract', OrderedDict([
+            ('doi', [jats('doi'), lambda v: "%s.001" % v, todo\
+                 ("is abstract doi logic cleverer than this?")]),
+            ('content', [jats('abstract'), todo("paragraphize this")])
+        ])),
+        
+    ])
+)])
 
 if __name__ == '__main__':
     doc = sys.argv[1]
