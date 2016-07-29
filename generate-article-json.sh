835a18c515e08d263d17599cb6d75b27199a1fce
Luke Skibinski <l.skibinski@elifesciences.org>
2016-07-29 17:55:49 +0100

    truncating scrape log before starting scrape

--- generate-article-json.sh
+++ generate-article-json.sh
@@ -2,6 +2,7 @@
 set -e
 source download-elife-xml.sh
 mkdir -p article-json
+echo > scrape.log
 for i in `ls ./article-xml/articles/*.xml | sort -r`; do 
     echo $i
     python ./src/main.py $i > ./article-json/$(basename $i).json
