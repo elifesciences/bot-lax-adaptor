f436b88a661e1f74a52dbff122e06d48787c6149
Luke Skibinski <l.skibinski@elifesciences.org>
2016-08-01 14:27:15 +0100

    added rough logic to validate.py. can't validate until I flesh out the json a bit more

--- src/validate.py
+++ src/validate.py
@@ -5,16 +5,21 @@ def json_load(fname):
     path = os.path.abspath(fname)
     return json.load(open(path, 'r'))
 
-def main(jsondoc):
+def is_poa(contents):
+    # todo: test, may be able to switch to 'status' in near future
+    return contents.has_key('body')
+
+def main(article):
     poa_schema = json_load('api-raml/dist/model/article-poa.v1.json')
     vor_schema = json_load('api-raml/dist/model/article-vor.v1.json')
-    contents = json_load(jsondoc)
+    contents = json_load(article)
 
-    print poa_schema
+    schema = vor_schema if is_poa(contents) else poa_schema
+    validate(contents, schema)
 
-    print vor_schema
-
-    print contents
+    #print poa_schema
+    #print vor_schema
+    #print contents
 
 if __name__ == '__main__':
     assert len(sys.argv) == 2, "the path to an article json file was expected"
