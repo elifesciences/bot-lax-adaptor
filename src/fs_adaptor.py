336997452750a1194eaa22d26afc8e91003a6950
Luke Skibinski <l.skibinski@elifesciences.org>
2016-09-19 19:08:18 +0100

    fs adaptor now properly yields a path with a 'file://' protocol

--- src/fs_adaptor.py
+++ src/fs_adaptor.py
@@ -1,6 +1,7 @@
 import os, json
-
+from os.path import join
 import logging
+
 LOG = logging.getLogger(__name__)
 
 class IncomingQueue(object):
@@ -8,18 +9,26 @@ class IncomingQueue(object):
         self.action = action
         self.dirname = dirname
 
-    def next(self):
-        for path in os.listdir(self.dirname):
+    def __iter__(self):
+        for fname in os.listdir(self.dirname):
+            if fname.startswith('.') or not fname.endswith('.xml'):
+                # hidden or not xml, skip
+                continue
+            path = join(self.dirname, fname)
             _, msid, ver = os.path.split(path)[-1].split('-') # ll: ['elife', '09560', 'v1.xml']
             ver = int(ver[1]) # "v1.xml" -> 1
+            LOG.debug("processing file: %s", path)
             yield json.dumps({
                 'action': self.action,
-                'location': path,
+                'location': 'file://' + path,
                 'id': msid,
                 'version': ver,
                 'force': False,
                 'token': 'pants-party'
             })
+
+    def close(self):
+        pass
 
 class OutgoingQueue(object):    
     def write(self, string):
