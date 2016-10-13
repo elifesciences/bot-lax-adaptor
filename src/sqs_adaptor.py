66637e0199bae7b06cbc8ba4eb83856889c2e3f3
Luke Skibinski <l.skibinski@elifesciences.org>
2016-10-13 13:15:10 +0100

    raised the boto logging level to WARN so we don't get all it's helpful-but-not-really error messages

--- src/sqs_adaptor.py
+++ src/sqs_adaptor.py
@@ -1,6 +1,10 @@
 import boto3
 import logging
 
+# tell boto to pipe down
+logging.getLogger('botocore').setLevel(logging.WARN)
+logging.getLogger('boto3').setLevel(logging.WARN) # CRITICAL)
+
 LOG = logging.getLogger(__name__)
 
 def conn():
