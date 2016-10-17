544cff31cca65714ce968d3d3b5e0520a4720414
Graham Nott <gnott@starglobal.ca>
2016-10-17 08:26:08 -0700

    Rewrite relatedArticles if present with a placeholder snippet.

--- src/validate.py
+++ src/validate.py
@@ -65,6 +65,42 @@ placeholder_authors = [{
 
 placeholder_statusDate = "1970-09-10T00:00:00Z"
 
+placeholder_related_article = {
+    "type": "research-article",
+    "status": "vor",
+    "id": "09561",
+    "version": 1,
+    "doi": "10.7554/eLife.09561",
+    "authorLine": "Paul HGM Dirks et al",
+    "title": "Geological and taphonomic context for the new hominin species <i>Homo naledi</i> from the Dinaledi Chamber, South Africa",
+    "published": "2015-09-10T00:00:00Z",
+    "statusDate": "2015-09-10T00:00:00Z",
+    "volume": 4,
+    "elocationId": "e09561",
+    "pdf": "https://elifesciences.org/content/4/e09561.pdf",
+    "subjects": [
+        "genomics-evolutionary-biology"
+    ],
+    "impactStatement": "A new hominin species found in a South African cave is part of one of the most unusual hominin fossil assemblages on record.",
+    "image": {
+        "alt": "",
+        "sizes": {
+            "2:1": {
+                "900": "https://placehold.it/900x450",
+                "1800": "https://placehold.it/1800x900"
+            },
+            "16:9": {
+                "250": "https://placehold.it/250x141",
+                "500": "https://placehold.it/500x281"
+            },
+            "1:1": {
+                "70": "https://placehold.it/70x70",
+                "140": "https://placehold.it/140x140"
+            }
+        }
+    }
+}
+
 def is_poa(contents):
     try:
         return contents["article"]["status"] == "poa"
@@ -85,6 +121,10 @@ def add_placeholders_for_validation(contents):
 
     contents['snippet']['statusDate'] = placeholder_statusDate
     contents['article']['statusDate'] = placeholder_statusDate
+
+    if 'relatedArticles' in contents['article']:
+        for i, value in enumerate(contents['article']['relatedArticles']):
+            contents['article']['relatedArticles'][i] = placeholder_related_article
 
     if not is_poa(contents):
         contents['article']['digest'] = placeholder_digest
