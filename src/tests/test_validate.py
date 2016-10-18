27cec9d79192d8372c73d9a7f96848851bab9092
Graham Nott <gnott@starglobal.ca>
2016-10-18 10:19:56 -0700

    More test coverage for validate.py

--- src/tests/test_validate.py
+++ src/tests/test_validate.py
@@ -32,7 +32,37 @@ class TestArticleValidate(BaseCase):
         validate.section_id_counter = None
         body_json = [{"type": "paragraph"}]
         wrapped_body_json = validate.wrap_body_in_section(body_json)
-        expected_body_json = [{'type': 'section', 'id': 'phantom-s-1', 'title': '', 'content': [{'type': 'paragraph'}]}]
+        expected_body_json = [{'type': 'section', 'id': 'phantom-s-1',
+                               'title': '', 'content': [{'type': 'paragraph'}]}]
         self.assertEqual(wrapped_body_json, expected_body_json)
 
+    def test_video_rewrite(self):
+        body_json = [{'type': 'video', 'uri': 'video.mov'}]
+        expected = [{'width': 640,
+                     'image': 'https://example.org/video.mov',
+                     'height': 480, 'sources':
+                     [{'mediaType': 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"',
+                       'uri': 'https://example.org/video.mov'}],
+                     'type': 'video'}]
+        self.assertEqual(validate.video_rewrite(body_json), expected)
 
+    def test_fix_image_attributes_if_missing(self):
+        body_json = [{'type': 'image'}]
+        expected = [{'type': 'image', 'title': 'This a placeholder for a missing image title'}]
+        self.assertEqual(validate.fix_image_attributes_if_missing(body_json), expected)
+
+    def test_fix_section_id_if_missing(self):
+        # Reset counter before test is run
+        validate.section_id_counter = None
+        body_json = [{'type': 'section'}]
+        expected = [{'type': 'section', 'id': 'phantom-s-1'}]
+        self.assertEqual(validate.fix_section_id_if_missing(body_json), expected)
+
+    def test_fix_box_title_if_missing(self):
+        body_json = [{'type': 'box'}]
+        expected = [{'type': 'box', 'title': 'Placeholder box title because we must have one'}]
+        self.assertEqual(validate.fix_box_title_if_missing(body_json), expected)
+
+    def test_is_poa_not_poa(self):
+        # For test coverage
+        self.assertFalse(validate.is_poa({}))
