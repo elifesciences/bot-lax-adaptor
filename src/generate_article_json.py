aa115062e3bad2a334efd21fa3243c0df2f7ae55
Luke Skibinski <l.skibinski@elifesciences.org>
2016-10-24 13:57:16 +0100

    the generate article-json script now uses the settings in conf.py

--- src/generate_article_json.py
+++ src/generate_article_json.py
@@ -8,11 +8,7 @@ import main as scraper
 import sys
 from StringIO import StringIO
 from joblib import Parallel, delayed
-
-SRC_DIR = os.path.dirname(os.path.abspath(__file__)) # ./bot-lax-adaptor/src/
-PROJECT_DIR = os.path.dirname(SRC_DIR)
-XML_DIR = join(PROJECT_DIR, 'article-xml', 'articles')
-JSON_DIR = join(PROJECT_DIR, 'article-json2')
+import conf
 
 def render(path):
     strbuffer = StringIO()
@@ -20,22 +16,22 @@ def render(path):
         fname = os.path.basename(path)
         strbuffer.write("%s -> %s => " % (fname, fname + '.json'))
         json_result = scraper.main(path)
-        outfname = join(JSON_DIR, fname + '.json')
+        outfname = join(conf.JSON_DIR, fname + '.json')
         open(outfname, 'w').write(json_result)
         strbuffer.write("success")
-    except Exception:
-        strbuffer.write("failed")
+    except Exception as err:
+        strbuffer.write("failed (%s)" % err)
     finally:
         sys.stderr.write(strbuffer.getvalue() + "\n")
         sys.stderr.flush()
 
 def main():
-    paths = map(lambda fname: join(XML_DIR, fname), os.listdir(XML_DIR))
+    paths = map(lambda fname: join(conf.XML_DIR, fname), os.listdir(conf.XML_DIR))
     paths = filter(lambda path: path.lower().endswith('.xml'), paths)
     paths = sorted(paths, reverse=True)
     #paths_sizes = zip(paths, map(os.path.getsize, paths))
     #map(render, paths)
-    Parallel(n_jobs=-1)(delayed(render)(path) for path in paths[:10])
+    Parallel(n_jobs=-1)(delayed(render)(path) for path in paths)
     print 'see scrape.log for errors'
 
 if __name__ == '__main__':
