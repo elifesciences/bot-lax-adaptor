709c1c1d62ea65b24cf999f811cc0c740bd1c0fd
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-16 12:45:25 +0000

    Need file:/// since one / is the absolute path

--- download-elife-xml.sh
+++ download-elife-xml.sh
@@ -9,7 +9,7 @@ if [ -d article-xml ]; then
     cd ..
 elif [ -d /home/elife/elife-article-xml ]; then
     # local copy on elife-libraries Jenkins node for faster cloning
-    git clone file://home/elife/elife-article-xml article-xml --depth 1
+    git clone file:///home/elife/elife-article-xml article-xml --depth 1
 else
     git clone https://github.com/elifesciences/elife-article-xml article-xml --depth 1
 fi
