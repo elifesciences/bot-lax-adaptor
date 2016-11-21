26cd8d3921bfeaeef70e5a70a4587e2ac8bc2845
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-21 17:10:43 +0000

    Counting the totals inside log-based statistics

--- generate-statistics.sh
+++ generate-statistics.sh
@@ -5,6 +5,7 @@ set -e
 log=$1
 generated_green=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml ->' $log | grep success | wc -l)
 generated_red=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml ->' $log | grep -v success | wc -l)
+generated_total=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml ->' $log | wc -l)
 input=$(ls -1 article-xml/articles/elife-* | wc -l)
 echo "Input: ${input} XML articles"
-echo "Generated green,red: ${generated_green},${generated_red}"
+echo "Generated green,red,total: ${generated_green},${generated_red},${generated_total}"
