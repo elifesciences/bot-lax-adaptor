586f03ded85f529635ec33e50fe733a3189666d6
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-21 16:32:20 +0000

    Expanding statistics with number of articles in input

--- generate-statistics.sh
+++ generate-statistics.sh
@@ -5,4 +5,6 @@ set -e
 log=$1
 generated_green=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml ->' $log | grep success | wc -l)
 generated_red=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml ->' $log | grep -v success | wc -l)
+input=$(ls -1 article-xml/articles/elife-* | wc -l)
+echo "Input: ${input} XML articles"
 echo "Generated green,red: ${generated_green},${generated_red}"
--- validate-statistics.sh
+++ validate-statistics.sh
@@ -5,4 +5,6 @@ set -e
 log=$1
 validated_green=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml\.json =>' $log | grep success | wc -l)
 validated_red=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml\.json =>' $log | grep -v success | wc -l)
+input=$(ls -1 article-json/elife-* | wc -l)
+echo "Input: ${input} JSON articles"
 echo "Validated green,red: ${validated_green},${validated_red}"
