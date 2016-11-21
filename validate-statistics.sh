a61566459a4a23d49ff34239fcb3904edaf4698b
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-21 17:29:08 +0000

    Extracted pattern for parsing validation log

--- validate-statistics.sh
+++ validate-statistics.sh
@@ -3,9 +3,11 @@
 set -e
 
 log=$1
-validated_green=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml\.json =>' $log | grep success | wc -l)
-validated_red=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml\.json =>' $log | grep -v success | wc -l)
-validated_total=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml\.json =>' $log | wc -l)
+article_pattern='^elife-[0-9]\+-v[0-9]\+\.xml\.json =>'
+
+validated_green=$(grep "$article_pattern" "$log" | grep success | wc -l)
+validated_red=$(grep "$article_pattern" "$log" | grep -v success | wc -l)
+validated_total=$(grep "$article_pattern" "$log" | wc -l)
 input=$(ls -1 article-json/elife-* | wc -l)
 echo "Input: ${input} JSON articles"
 echo "Validated green,red: ${validated_green},${validated_red},${validated_total}"
