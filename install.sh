ae4e259ee55ed54437b01c5ca014a7f7915256ea
Luke Skibinski <l.skibinski@elifesciences.org>
2016-11-21 16:55:09 +0000

    added backfill script. added ability to specify directory of xml to generate, defaulting to github repo

--- install.sh
+++ install.sh
@@ -9,6 +9,5 @@ fi
 source venv/bin/activate
 
 pip install -r requirements.txt
-pip install git+https://github.com/elifesciences/elife-tools.git@develop#egg=elifetools --upgrade
 
 . download-api-raml.sh
