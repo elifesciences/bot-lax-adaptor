213f6581f0ae5ed0e1a35e0f6b89f4f583d5e6a2
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-21 17:28:52 +0000

    Added check on number of articles failing generation

--- generate-statistics.sh
+++ generate-statistics.sh
@@ -3,9 +3,16 @@
 set -e
 
 log=$1
-generated_green=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml ->' $log | grep success | wc -l)
-generated_red=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml ->' $log | grep -v success | wc -l)
-generated_total=$(grep '^elife-[0-9]\+-v[0-9]\+\.xml ->' $log | wc -l)
+article_pattern='^elife-[0-9]\+-v[0-9]\+\.xml ->'
+
+generated_green=$(grep "$article_pattern" "$log" | grep success | wc -l)
+generated_red=$(grep "$article_pattern" "$log" | grep -v success | wc -l)
+generated_total=$(grep "$article_pattern" "$log" | wc -l)
 input=$(ls -1 article-xml/articles/elife-* | wc -l)
 echo "Input: ${input} XML articles"
 echo "Generated green,red,total: ${generated_green},${generated_red},${generated_total}"
+if [ "$generated_red" -gt 0 ]; then
+    echo "No articles should be failing generation"
+    grep "${article_pattern}" "$log" | grep -v success
+    exit $generated_red
+fi
