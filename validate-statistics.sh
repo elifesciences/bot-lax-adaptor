a7ea2105eccd4273fdd5624f901e06bee926e26c
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-21 17:31:08 +0000

    Added check on maximum invalid articles

--- validate-statistics.sh
+++ validate-statistics.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# print statistics on the generation by checking its stdout
+# print statistics on the validation by checking its stdout
 set -e
 
 log=$1
+maximum_invalid=78
 article_pattern='^elife-[0-9]\+-v[0-9]\+\.xml\.json =>'
 
 validated_green=$(grep "$article_pattern" "$log" | grep success | wc -l)
@@ -11,3 +12,9 @@ validated_total=$(grep "$article_pattern" "$log" | wc -l)
 input=$(ls -1 article-json/elife-* | wc -l)
 echo "Input: ${input} JSON articles"
 echo "Validated green,red: ${validated_green},${validated_red},${validated_total}"
+if [ "$validated_red" -gt "$maximum_invalid" ]; then
+    echo "No more than $maximum_invalid articles should be failing validation (with placeholders)"
+    grep "${article_pattern}" "$log" | grep -v success
+    exit $validated_red
+fi
+
