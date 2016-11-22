2b2e21311bbc0f7e0cca6b9992e408c508c957c0
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-22 11:07:51 +0000

    Have to skip shallow cloning to be able to force a git revision of elife-article-xml

--- download-elife-xml.sh
+++ download-elife-xml.sh
@@ -9,9 +9,9 @@ if [ -d article-xml ]; then
     cd ..
 elif [ -d /home/elife/elife-article-xml ]; then
     # local copy on elife-libraries Jenkins node for faster cloning
-    git clone file:///home/elife/elife-article-xml article-xml --depth 1
+    git clone file:///home/elife/elife-article-xml article-xml
 else
-    git clone https://github.com/elifesciences/elife-article-xml article-xml --depth 1
+    git clone https://github.com/elifesciences/elife-article-xml article-xml
 fi
 
 if [ -f elife-article-xml.sha1 ]; then
