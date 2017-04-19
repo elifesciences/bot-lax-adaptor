c0605e16d6b2a051c3b948a514f5077b311086d9
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-19 13:50:44 +0100

    Add install step before updating Pip dependencies

--- update-elife-tools.sh
+++ update-elife-tools.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+. install.sh 1>&2
+
 rm -rf /tmp/elife-tools-shallow-clone
 git clone --depth 1 https://github.com/elifesciences/elife-tools.git /tmp/elife-tools-shallow-clone
 cd /tmp/elife-tools-shallow-clone
@@ -8,7 +10,6 @@ cd /tmp/elife-tools-shallow-clone
 elife_tools_sha1=$(git rev-parse HEAD)
 cd - 1>&2
 sed -i -e "s;.*/elife-tools.git.*;git+https://github.com/elifesciences/elife-tools.git@${elife_tools_sha1}#egg=elifetools;g" requirements.txt
-source venv/bin/activate
 pip uninstall -y elifetools 1>&2
 pip install -r requirements.txt 1>&2
 echo "requirements.txt pinned to $elife_tools_sha1"
