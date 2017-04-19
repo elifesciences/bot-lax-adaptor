15fb4c26de2bbdb262b0b44a689ce6b889058be4
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-19 13:18:01 +0100

    Pipeline for updating elife-tools

--- update-elife-tools.sh
+++ update-elife-tools.sh
@@ -6,9 +6,9 @@ git clone --depth 1 https://github.com/elifesciences/elife-tools.git /tmp/elife-
 cd /tmp/elife-tools-shallow-clone
 # this uses the develop branch, which is the default being cloned
 elife_tools_sha1=$(git rev-parse HEAD)
-cd -
+cd - 1>&2
 sed -i -e "s;.*/elife-tools.git.*;git+https://github.com/elifesciences/elife-tools.git@${elife_tools_sha1}#egg=elifetools;g" requirements.txt
 source venv/bin/activate
-pip uninstall -y elifetools
-pip install -r requirements.txt
-echo "(requirements.txt updated with $elife_tools_sha1)"
+pip uninstall -y elifetools 1>&2
+pip install -r requirements.txt 1>&2
+echo "requirements.txt pinned to $elife_tools_sha1"
