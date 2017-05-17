e4548db45d5346c55fe3086aa6dfda4b521b50d5
Graham Nott <gnott@starglobal.ca>
2017-05-16 18:39:33 -0700

    Include the other missing module from previous commits.

---
+++ src/cdn.py
@@ -0,0 +1,31 @@
+import requests, requests_cache
+from cache_requests import install_cache_requests
+import logging
+
+LOG = logging.getLogger(__name__)
+install_cache_requests()
+
+def url_exists(url, msid=None):
+    context = {'msid': msid, 'url': url}
+
+    try:
+        resp = requests.head(url)
+    except requests.ConnectionError:
+        LOG.debug("CDN request failed", extra=context)
+        return None
+
+    context['status-code'] = resp.status_code
+
+    if resp.status_code == requests.codes.ok:
+        return url
+    elif resp.status_code == 404:
+        LOG.debug("CDN url not found", extra=context)
+    else:
+        msg = "unhandled status code from CDN"
+        LOG.warn(msg, extra=context)
+        raise ValueError(msg + ": %s" % resp.status_code)
+
+    return None
+
+def clear_cache(url):
+    requests_cache.core.get_cache().delete_url(url)
