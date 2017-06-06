57308d6a29858d9c32cd52aac60b9a8d1facb75c
Luke Skibinski <l.skibinski@elifesciences.org>
2017-06-06 11:44:09 +0930

    updated example backfill script to use the new script name

--- backfill-example.sh
+++ backfill-example.sh
@@ -2,7 +2,7 @@
 set -ev
 
 # pass json objects to backfill script, one per line:
-cat example.txt | jq . --compact-output | python src/backfill.py
+cat example.txt | jq . --compact-output | python src/adhoc_backfill.py --dry-run
 
 # pass filenames to backfill script (as a dry run)
-python src/backfill.py article-xml/articles/elife-09560-v1.xml article-xml/articles/elife-09561-v1.xml --dry-run
+python src/adhoc_backfill.py article-xml/articles/elife-09560-v1.xml article-xml/articles/elife-09561-v1.xml --dry-run
