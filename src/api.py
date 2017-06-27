7f33c01abc668a6248b10eee36037e546bf25376
Luke Skibinski <l.skibinski@elifesciences.org>
2017-06-27 16:59:41 +0930

    api more focused now. first test ported over.

--- src/api.py
+++ src/api.py
@@ -1,28 +1,55 @@
-import os
+import os, json
 from os.path import join
 import conf
 from flex.core import validate
 import flex
 import connexion
+import flask
 from flask import request
 import main as scraper, validate as ajson_validate
 import jsonschema
 from connexion.resolver import RestyResolver
+from werkzeug.exceptions import HTTPException
+
+# just the default, can be overriden when creating an app
+UPLOAD_FOLDER = join(conf.PROJECT_DIR, 'web', 'uploads') 
 
 #
-#
+# utils
 #
 
-def validates(spec):
+def validate_schema():
+    "validates the api scheme"
     path = join(conf.PROJECT_DIR, 'schema', 'api.yaml')
     spec = flex.load(path)
     validate(spec)
     return True
 
+def http_exception(code, msg):
+    "returns an HTTPException object with the correct code and message set"
+    ex = HTTPException(msg)
+    ex.code = code
+    return ex
+
+def http_ensure(case, msg, code=400):
+    "if not case, raise a client error exception (by default)"
+    if not case:
+        raise http_exception(code, msg)
+
+def listfiles(path, ext_list=None):
+    "returns a pair of (basename_list, absolute_path_list) for given dir, optionally filtered by extension"
+    path_list = map(lambda fname: os.path.abspath(join(path, fname)), os.listdir(path))
+    if ext_list:
+        path_list = filter(lambda path: os.path.splitext(path)[1] in ext_list, path_list)
+    path_list = sorted(filter(os.path.isfile, path_list))
+    return map(os.path.basename, path_list), path_list
 
 #
 #
 #
+
+def upload_folder():
+    return flask.current_app.config['UPLOAD_FOLDER']
 
 class AsdfResolver(RestyResolver):
     """custom resolver that flattens func name lookup after initial module.
@@ -37,99 +64,93 @@ class AsdfResolver(RestyResolver):
         # the funname resolution is wonky, so discard it
         method = bits[-1] # ll: 'search'
 
-        path = operation.path # ll: /ajson/validation/{filename}
+        path = operation.path # ll: /article-json/validation/{filename}
         # we want something like:
-        # /ajson/validation/{filename} => ajson_validation
-        # /ajson/{filename}/validation => ajson_validation
-        bits = path.strip('/').split('/') # => ['ajson', 'validation', '{filename}']
+        # /article-json/validation/{filename} => article_json_validation
+        # /article-json/{filename}/validation => article_json_validation
+        bits = path.strip('/').split('/') # => ['article-json', 'validation', '{filename}']
         bits = filter(lambda bit: bit and not bit.startswith('{') and not bit.endswith('}'), bits)
-        fnname = '_'.join(bits) # ll: ajson_validation
-        return '%s.%s_%s' % (module, method, fnname) # ll: api.post_ajson_validation
+        fnname = '_'.join(bits).replace('-', '_') # ll: article_json_validation
+        return '%s.%s_%s' % (module, method, fnname) # ll: api.post_article_json_validation
 
+def article_json_files():
+    return listfiles(upload_folder(), ext_list=['.json'])
 
-def listfiles(path, ext_list=None):
-    "returns a list of absolute paths for given dir"
-    path_list = map(lambda fname: os.path.abspath(join(path, fname)), os.listdir(path))
-    if ext_list:
-        path_list = filter(lambda path: os.path.splitext(path)[1] in ext_list, path_list)
-    return sorted(filter(os.path.isfile, path_list))
-
+def xml_files():
+    return listfiles(upload_folder(), ext_list=['.xml'])
 
 #
 # api
 #
 
-UPLOAD_FOLDER = join(conf.PROJECT_DIR, 'web', 'uploads')
-NOT_IMPLEMENTED = {}, 501
-
 def search_xml():
-    paths = listfiles(UPLOAD_FOLDER, ext_list=['.xml'])
-    return map(os.path.basename, paths)
+    "GET /xml"
+    return map(os.path.basename, xml_files())
+
+def search_article_json():
+    return article_json_files()[0] # just the basenames
+
+def get_article_json(filename):
+    basenames, full_paths = article_json_files()
+    if not filename:
+        return basenames
+    http_ensure(filename in basenames, "requested file not found", 404)
+    idx = dict(zip(basenames, full_paths))
+    return json.load(open(idx[filename], 'r'))
 
 def post_xml():
-    "upload jats xml, generate xml, validate"
-    if not 'xml' in request.files:
-        #flash('xml file not found')
-        # return redirect("/")
-        return {'error': 'not found'}
+    "upload jats xml, generate xml, validate, send to lax as a dry run"
+    http_ensure('xml' in request.files, "xml file required", 404)
 
     # upload
     xml = request.files['xml']
-    filename = xml.filename # todo: sanitize this
-    if not os.path.splitext(filename)[1] == '.xml':
-        #flash("only xml uploads, please")
-        # return redirect("/")
-        return {'error': "file doesn't look like xml"}
-    path = join(UPLOAD_FOLDER, filename)
+    filename = xml.filename # todo: sanitize this. assumes a name like 'elife-00000-v1.xml'
+    http_ensure(os.path.splitext(filename)[1] == '.xml', "file doesn't look like xml")
+    path = join(upload_folder(), filename)
     xml.save(path)
 
     # generate
     try:
-        # print 'generating'
         results = scraper.main(path)
         json_filename = filename + '.json'
-        json_path = join(UPLOAD_FOLDER, json_filename)
+        json_path = join(upload_folder(), json_filename)
         open(json_path, 'w').write(results)
-        #flash("generated %s" % json_filename)
     except Exception as err:
-        # flash(err)
-        # return redirect("/")
         return {'error': str(err)}
 
     # validate
     try:
-        # print 'validating'
         ajson_validate.main(open(json_path, 'r'))
-        # flash("valid")
-        return {'error': 'success'}
+        return results
+
     except jsonschema.ValidationError as err:
-        #flash("invalid: " + str(err))
-        return {'error': 'invalid'}
+        return {
+            'error': 'invalid-article-json',
+            'details': str(err), # todo: any good?
+        }
 
-    # return redirect("/")
+    # send to lax
+    # ...
 
-def search_ajson():
-    paths = listfiles(UPLOAD_FOLDER, ext_list=['.json'])
-    return map(os.path.basename, paths)
-
-def post_ajson_validate():
-    return NOT_IMPLEMENTED
-
-def post_ajson_generate(filename):
-    return NOT_IMPLEMENTED
-
-def main():
-    APP = connexion.App(__name__, specification_dir=join(conf.PROJECT_DIR, 'schema'))
-    APP.add_api('api.yaml', resolver=AsdfResolver('api'))
-    APP.app.config.update({
-        #'DEBUG': True,
-        'SECRET_KEY': os.urandom(24),
+def create_app(cfg_overrides):
+    app = connexion.App(__name__, specification_dir=join(conf.PROJECT_DIR, 'schema'))
+    app.add_api('api.yaml', resolver=AsdfResolver('api'))
+    cfg = {
+        'SECRET_KEY': os.urandom(24), # necessary for uploads
         # http://flask.pocoo.org/docs/0.11/config/#instance-folders
         'UPLOAD_FOLDER': UPLOAD_FOLDER,
+    }
+    cfg.update(cfg_overrides)
+    app.app.config.update(cfg)
+    return app
+
+def main():
+    app = create_app({
+        #'DEBUG': True,
         # Flask/Jinja caches templates
         'TEMPLATES_AUTO_RELOAD': True
     })
-    APP.run(port=8080)  # , #debug=True)
+    app.run(port=8080)
 
 if __name__ == '__main__':
     main()
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,6 +1,7 @@
+
 from os.path import join
 from .base import BaseCase
-import conf, api
+import api
 
 class One(BaseCase):
     def setUp(self):
@@ -11,5 +12,144 @@ class One(BaseCase):
         pass
 
     def test_schema_validates(self):
-        spec = conf.API_SCHEMA
-        self.assertTrue(api.validates(spec))
+        self.assertTrue(api.validate_schema())
+
+    '''
+    # this approach has promise, but all the libraries in this domain are woefully
+    # implemented and maintained. I've had to modify swagger-tester and swagger-parser
+    # quite a bit aleady just to get this far. I need:
+    # * the ability to override the test for certain cases.
+    # * the ability to continue testing other paths/methods/parameters if any one fails
+    # * the ability to setup and teardown state for any given test
+    def test_foo(self):
+        import conf
+        from swagger_tester import swagger_test
+        path = join(conf.PROJECT_DIR, 'schema', 'api.yaml')
+        print 'path',path
+        swagger_test(path, resolver=api.AsdfResolver('api'))
+    '''
+
+class Two(BaseCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+import os, shutil, tempfile
+from flask_testing import TestCase
+
+class Web(TestCase):
+    maxDiff = None
+    this_dir = os.path.realpath(os.path.dirname(__file__))
+    fixtures_dir = join(this_dir, 'fixtures')
+
+    render_templates = False
+
+    def create_app(self):
+        self.temp_dir = tempfile.mkdtemp(suffix='bot-lax-api-test')
+        cxx = api.create_app({
+            'DEBUG': True,
+            'UPLOAD_FOLDER': self.temp_dir
+        })
+        return cxx.app
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
+
+    def test_upload_valid(self):
+        xml_fname = 'elife-16695-v1.xml'
+        xml_fixture = join(self.fixtures_dir, xml_fname)
+
+        resp = self.client.post('/xml', **{
+            'buffered': True,
+            'content_type': 'multipart/form-data',
+            'data': {
+                'xml': (open(xml_fixture, 'r'), xml_fname),
+            }
+        })
+        self.assertEqual(resp.status_code, 200)
+        expected_path = join(self.temp_dir, xml_fname)
+        self.assertTrue(os.path.exists(expected_path), "uploaded xml cannot be found")
+        self.assertTrue(os.path.isfile(expected_path), "uploaded xml is not a file??")
+
+'''
+    def test_upload_invalid(self):
+        xml_fname = 'elife-00666-v1-invalid.xml'
+        xml_fixture = join(self.fixtures_dir, xml_fname)
+
+        self.client.post('/upload/', **{
+            'buffered': True,
+            'content_type': 'multipart/form-data',
+            'data': {
+                # data, filename
+                'xml': (open(xml_fixture, 'r'), 'elife-00666-v1.xml'),
+            }
+        })
+        expected_path = join(self.temp_dir, 'elife-00666-v1.xml')
+        self.assertTrue(os.path.exists(expected_path))
+        self.assertTrue(os.path.isfile(expected_path))
+        self.assert_flashes(lambda message: message.startswith('invalid'))
+
+    def test_generate_file(self):
+        # upload file
+        xml_fname = 'elife-16695-v1.xml'
+        xml_fixture = join(self.fixtures_dir, xml_fname)
+        self.client.post('/upload/', **{
+            'buffered': True,
+            'content_type': 'multipart/form-data',
+            'data': {
+                'xml': (open(xml_fixture, 'r'), xml_fname),
+            }
+        })
+        #expected_path = join(self.temp_dir, xml_fname)
+        # re-generate
+        self.client.post('/generate/' + xml_fname, **{
+            'data': {
+                'filename': xml_fname,
+            }
+        })
+        self.assert_flashes(lambda message: message.startswith('regenerated'))
+
+    def test_validate_non_json_file(self):
+        # upload file
+        xml_fname = 'elife-16695-v1.xml'
+        xml_fixture = join(self.fixtures_dir, xml_fname)
+        self.client.post('/upload/', **{
+            'buffered': True,
+            'content_type': 'multipart/form-data',
+            'data': {
+                'xml': (open(xml_fixture, 'r'), xml_fname),
+            }
+        })
+        #expected_path = join(self.temp_dir, xml_fname)
+        # re-generate
+        self.client.post('/validate/' + xml_fname, **{
+            'data': {
+                'filename': xml_fname,
+            }
+        })
+        self.assert_flashes(lambda message: message.startswith('not validated'))
+
+    def test_validate_json_file(self):
+        # upload file
+        xml_fname = 'elife-16695-v1.xml'
+        xml_fixture = join(self.fixtures_dir, xml_fname)
+        self.client.post('/upload/', **{
+            'buffered': True,
+            'content_type': 'multipart/form-data',
+            'data': {
+                'xml': (open(xml_fixture, 'r'), xml_fname),
+            }
+        })
+
+        json_fname = xml_fname + '.json'
+        self.client.post('/validate/' + json_fname, **{
+            'data': {
+                'filename': json_fname,
+            }
+        })
+        self.assert_flashes(lambda message: message.startswith('valid'))
+
+'''
