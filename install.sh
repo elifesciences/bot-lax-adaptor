bf0bb50c80704f44e988c923fe9223ebc199e565
Giorgio Sironi <g.sironi@elifesciences.org>
2018-08-07 11:31:15 +0100

    Create virtualenv using virtualenv binary to avoid ensurepip failure

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ py=${python##*/} # ll: python3.6
 if [ ! -e "venv/bin/$py" ]; then
     echo "could not find venv/bin/$py, recreating venv"
     rm -rf venv
-    $python -m venv venv
+    virtualenv --python=$python venv/
 fi
 
 source venv/bin/activate
