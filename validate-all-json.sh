8c557fcf055c68969f11aa55600d541171996650
Luke <2142748+lsh-0@users.noreply.github.com>
2023-10-24 15:34:33 +1030

    validate-all-json.sh, detects arch and fetches correct release

--- validate-all-json.sh
+++ validate-all-json.sh
@@ -2,7 +2,7 @@
 # validates the article-json found in the `./article-json` directory.
 # this directory is populated by the `generate-article-json.sh` script.
 
-set -e
+set -ex
 
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
@@ -11,16 +11,32 @@ function ctrl_c() {
 }
 
 # clean up
-rm -f linux-amd64
+rm -f linux-amd64 linux-arm64 validate-article-json
 rm -f validation.log validate.log
 rm -rf ./article-json/valid/ ./article-json/invalid/  ./article-json/patched/
 
 # fetch latest validator
-wget https://github.com/elifesciences/validate-article-json/releases/latest/download/linux-amd64
-chmod +x linux-amd64
-mv linux-amd64 validate-article-json
+arch=$(uname -m)
+fname=""
+if [ "$arch" = "x86_64" ]; then
+    fname="linux-amd64"
+fi
+
+# aka "ARM64"
+if [ "$arch" = "aarch64" ]; then
+    fname="linux-arm64"
+fi
+
+if [ -z "$fname" ]; then
+    echo "unsupported architecture: $arch"
+    exit 1
+fi
+
+wget "https://github.com/elifesciences/validate-article-json/releases/latest/download/$fname" \
+    --quiet \
+    --output-document=validate-article-json
+chmod +x validate-article-json
 
 # validate
 sample="-1"      # 'all' articles (do not sample)
-num_workers="72" # 36 cores (elife-libraries--powerful3) * 2
-time ./validate-article-json schema/api-raml/ article-json/ "$sample" "$num_workers"
+time ./validate-article-json schema/api-raml/ article-json/ "$sample"
